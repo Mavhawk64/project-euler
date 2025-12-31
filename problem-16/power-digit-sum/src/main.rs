baabc930573ed194f8610fb4aadd4405dfbf8bcc
Mavhawk64 <mavberk@gmail.com>
2025-12-30 22:58:01 -0500

    Update main.rs

--- problem-16/power-digit-sum/src/main.rs
+++ problem-16/power-digit-sum/src/main.rs
@@ -1,9 +1,42 @@
 fn main() {
     // What is the sum of the digits of 2^1000 ?
-    println!("{}", u128::MAX); // much smaller than 2**1000...
+    // println!("{}", u128::MAX); // much smaller than 2**1000...
 
     // I think we need to either do some base magic or binary magic...
+    // (one day later) maybe not...
+    // I did a LeetCode problem with LinkNodes (like a number, 1234 being represented as 4->3->2->1)
+    // and I was thinking I would create my own version of this for this problem and just do the digit by digit doubling
+    // but then I thought, "I can do this with an array".
+    // So here is a solution with an array that doubles the digits and progressively adds upon the previous list of digits.
+    println!("{}", sum_2_pow(1000));
 }
+
+fn sum_2_pow(n: u32) -> u32 {
+    let mut digits: Vec<u8> = vec![1];
+
+    for _ in 0..n {
+        let mut carry: u8 = 0;
+
+        for d in digits.iter_mut() {
+            let t: u16 = 2 * (*d as u16) + (carry as u16);
+            *d = (t % 10) as u8;
+            carry = (t / 10) as u8;
+        }
+
+        if carry != 0 {
+            digits.push(carry);
+        }
+    }
+
+//     let mut o: u128=0;
+//     for &d in digits.iter() {
+//     o += d as u128;
+// }
+// same result as below w/ mapping each val to u128:
+
+    return digits.iter().map(|&d| d as u32).sum(); // a little sum magic w/ map courtesy of ChatGPT
+}
+
 
 // Python 3.14.1 (main, Dec  2 2025, 12:51:37) [GCC 12.3.0] on linux
 // Type "help", "copyright", "credits" or "license" for more information.
