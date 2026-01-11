ea2b7bb27031f1cb585cfccf9c8066210a5950d1
Maverick Berkland <mavberk@gmail.com>
2026-01-10 22:36:16 -0500

    Problem 1

--- solutions_1-50/problem-1/multiples-of-3-or-5/src/main.rs
+++ solutions_1-50/problem-1/multiples-of-3-or-5/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum: u32 = 0;
+    for i in 0..1000 {
+        sum += if i % 3 == 0 || i % 5 == 0 { i } else { 0 };
+    }
+    println!("{}", sum);
 }
