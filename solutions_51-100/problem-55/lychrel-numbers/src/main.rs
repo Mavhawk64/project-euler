bd29f16553bbcb404f65374ca02afde7514e5287
Maverick Berkland <mavberk@gmail.com>
2026-01-12 00:59:01 -0500

    Problem 55

--- solutions_51-100/problem-55/lychrel-numbers/src/main.rs
+++ solutions_51-100/problem-55/lychrel-numbers/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let mut lychrel_nums: Vec<u128> = Vec::new();
+    for i in 1..10_001 {
+        let mut is_lychrel: bool = true;
+        let mut num: u128 = i;
+        for _ in 0..50 {
+            let reversed_num: u128 = num
+                .to_string()
+                .chars()
+                .rev()
+                .collect::<String>()
+                .parse()
+                .unwrap();
+            num = reversed_num + num;
+            if is_palindrome(num.to_string()) {
+                is_lychrel = false;
+                break;
+            }
+        }
+        if is_lychrel {
+            lychrel_nums.push(i);
+        }
+    }
+    println!("{:?}\n{}", lychrel_nums, lychrel_nums.len());
+}
+
+fn is_palindrome(x: String) -> bool {
+    let mut c: Vec<char> = x.chars().collect();
+    while !c.is_empty() {
+        if c.len() == 1 {
+            break;
+        }
+        let f: char = c.remove(0);
+        let e: Option<char> = c.pop();
+        if Some(f) != e {
+            return false;
+        }
+    }
+    true
 }
