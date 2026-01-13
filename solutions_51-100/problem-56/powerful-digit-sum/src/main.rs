6682e6daed47e850f6d9be67bdd2a9c9659225ff
Maverick Berkland <mavberk@gmail.com>
2026-01-12 21:36:04 -0500

    Problem 56

--- solutions_51-100/problem-56/powerful-digit-sum/src/main.rs
+++ solutions_51-100/problem-56/powerful-digit-sum/src/main.rs
@@ -1,3 +1,20 @@
+use num_bigint::BigUint;
+
 fn main() {
-    println!("Hello, world!");
+    let mut max: u16 = 0;
+    for a in 1u32..100 {
+        for b in 1u32..100 {
+            let base = BigUint::from(a);
+            let num: u16 = base
+                .pow(b)
+                .to_string()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u16)
+                .sum();
+            if max < num {
+                max = num;
+            }
+        }
+    }
+    println!("{}", max);
 }
