6a29435806daf4de548a53cfc07f9f055e8bba78
Maverick Berkland <mavberk@gmail.com>
2026-01-15 22:59:59 -0500

    Problem 57

--- solutions_51-100/problem-57/square-root-convergents/src/main.rs
+++ solutions_51-100/problem-57/square-root-convergents/src/main.rs
@@ -1,3 +1,24 @@
+use num_bigint::BigUint;
+use num_rational::Ratio;
+use num_traits::One;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", sqrt2(1000));
+}
+
+fn sqrt2(depth: usize) -> usize {
+    // 1 + 1/2 -> depth => 1
+    // 1 +  1/(1 + (1 + 1/2)) -> depth - 1 => 2 ...
+    let mut ret: usize = 0;
+    let mut rat: Ratio<BigUint> = Ratio::new(BigUint::from(3u32), BigUint::from(2u32));
+    // println!("{} / {}", rat.numer(), rat.denom());
+    for _ in 2..=depth {
+        let one: Ratio<BigUint> = Ratio::one();
+        rat = one.clone() + one.clone() / (one.clone() + rat);
+        // println!("{} / {}", rat.numer(), rat.denom());
+        if rat.numer().to_string().len() > rat.denom().to_string().len() {
+            ret += 1;
+        }
+    }
+    ret
 }
