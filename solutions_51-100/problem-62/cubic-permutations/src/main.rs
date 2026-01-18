550b9bfe2182af74666aa0346678a59d54dee541
Mavhawk64 <mavberk@gmail.com>
2026-01-18 13:30:11 -0500

    Problem 62

--- solutions_51-100/problem-62/cubic-permutations/src/main.rs
+++ solutions_51-100/problem-62/cubic-permutations/src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cubes: HashMap<[u8; 10], Vec<u64>> = HashMap::new();
+
+    for n in 1..10_000 {
+        let c = cube(n);
+        let signature = num_to_digit_count(c);
+        cubes.entry(signature).or_default().push(n);
+        if cubes[&signature].len() == 5 {
+            let first_root = cubes[&signature][0];
+            println!("Found 5 permutations! Smallest cube: {}", cube(first_root));
+            break;
+        }
+    }
+}
+
+fn cube(n: u64) -> u128 {
+    (n as u128).pow(3)
+}
+
+fn num_to_digit_count(mut n: u128) -> [u8; 10] {
+    let mut counts = [0u8; 10]; // malloc 10 slots of size u8, initialized to 0u8.
+    while n > 0 {
+        counts[(n % 10) as usize] += 1;
+        n /= 10;
+    }
+    counts
 }
