00ea39d4f956a89ac9877af1939651948c39e4fd
Mavhawk64 <mavberk@gmail.com>
2026-01-18 13:57:00 -0500

    Problem 63

--- solutions_51-100/problem-63/powerful-digit-counts/src/main.rs
+++ solutions_51-100/problem-63/powerful-digit-counts/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s: u32 = 0;
+    for a in 1..10 {
+        s += count(a);
+    }
+    println!("{}", s);
+}
+
+// with a little bit of math, we can find:
+// 10^{n-1}\leq a^n is our bound
+// 1-1/n\leq\log_{10}a
+// Max: a = 9:
+// n\approx 21.###
+// therefore, n = 21 is our upper bound.
+// we can generate our counts with that formula above...
+// n\leq\frac{1}{1-\log_{10}a}
+// count(a) = \floor{\frac{1}{1-\log_{10}a}}
+fn count(a: u32) -> u32 {
+    (1f32 / (1f32 - (a as f32).log10())) as u32
 }
